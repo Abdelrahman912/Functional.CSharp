66922d20550bdf2cfe389f3154a2a95056caf7f3
abdelrahman fathy <abdelrahmanfathe95@gmail.com>
2021-06-20 21:57:22 +0200

    init solution.

---
+++ Functional.Core/Extensions/OptionExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace Functional.Core.Extensions
+{
+    public static class OptionExtension
+    {
+        public static Option.None None =>
+            Option.None.Default;
+
+        public static Option.Some<T> Some<T>(T value) =>
+            new Option.Some<T>(value);
+    }
+}
---
+++ Functional.Core/Option.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static Functional.Core.Extensions.OptionExtension;
+
+namespace Functional.Core
+{
+
+    public struct Option<T>
+    {
+        private readonly bool _isSome;
+        private readonly T _value;
+
+        private Option(T value)
+        {
+            _isSome = true;
+            _value = value;
+        }
+
+        public static implicit operator Option<T>(Option.None _) =>
+            new Option<T>();
+
+        public static implicit operator Option<T>(Option.Some<T> some) =>
+            new Option<T>(some.Value);
+
+        public static implicit operator Option<T>(T value) =>
+           value == null ? None : Some(value);
+
+        public R Match<R>(Func<R> none, Func<T, R> some) =>
+            _isSome == true ? some(_value) : none();
+
+    }
+
+    namespace Option
+    {
+        public struct None
+        {
+            internal static readonly None Default = new None();
+        }
+
+        public struct Some<T>
+        {
+            internal T Value { get; }
+            public Some(T value)
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+                Value = value;
+            }
+        }
+
+    }
+}
